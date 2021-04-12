32b9e5499644f7bdaa970350be1d9d5ee6716be7
Boryana Georgieva <borqna_georgieva@abv.bg>
2021-04-12 20:36:04 +0300

    add create post page

--- src/pages/sharePost/index.js
+++ src/pages/sharePost/index.js
@@ -1,57 +1,38 @@
-// import React, { useState } from 'react'
-// import styles from './index.module.css'
-// import Container from '../../components/container'
-// import Title from '../../components/title'
-// import Posts from '../../components/posts'
-// import Submit from '../../components/submit-button'
-// import getCookie from '../../utils/cookie'
-// import Input from '../../components/input'
+import React from 'react'
+import Button from '../../components/button'
+import Input from '../../components/input'
+import styles from './index.module.css'
+import Title from '../../components/title'
+import Textarea from '../../components/textarea'
 
-// const SharePost = () => {
-//     const [description, setDescription] = useState('')
-//     const [title, setTitle] = useState([])
+const SharePost = () => {
 
-//     const onSubmit = async () => {
-//         await fetch('http://localhost:9999/api/post', {
-//             method: 'POST',
-//             body: JSON.stringify({
-//                 title: title,
-//                 description: description
-//             }),
-//             headers: {
-//                 'Content-Type': 'application/json',
-//                 'Authorization': getCookie('x-auth-token')
-//             }
-//         })
 
-//         setDescription('')
-//         setTitle('')
-//     }
 
-//     return (
-//         <Container>
-//             <Title title="Добавете статия" />
-//             <section className={styles.create}>
+    return (
+        <section className={styles.create}>
+            <Title title="Добавете статия" />
+            <form id="createPost">
+                <fieldset>
+                    <Input
+                        value="title"
+                        label="Заглавие"
+                        id="title"
+                    />
+                    <Textarea
+                        label="Статия:"
+                        id="description"
+                        placeholder="Въведете текста на статията:"
+                    />
+                    <Button
+                        type="submit"
+                        title="Добави"
+                        className="createPost"
+                    />
+                </fieldset>
+            </form>
+        </section>
+    )
+}
 
-//                 <fieldset>
-//                     <legend>Добавете статия</legend>
-//                     <p className="title">
-//                         <Input
-//                             value={title}
-//                             onChange={e => setTitle(e.target.value)}
-//                             label="Заглавие"
-//                             id="title" />
-//                     </p>
-//                     <p className="description">Статия:  
-//                         <textarea  name="description" rows="5" cols="33" />
-//                     </p>
-//                     <Submit title="Добави" onClick={onSubmit} />
-
-//                 </fieldset>
-//             </section>
-
-//         </Container>
-//     )
-// }
-
-// export default SharePost
+export default SharePost
