36e913cd27bcfbf7ea026af11b61e05ec88c2548
Boryana Georgieva <borqna_georgieva@abv.bg>
2021-04-14 23:38:24 +0300

    change login page with hook

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,27 +1,50 @@
-import React from 'react'
+import React, { useState, useContext } from 'react'
+import { useHistory } from "react-router-dom"
 import Button from '../../components/button'
 import Input from '../../components/input'
 import Title from '../../components/title'
 import styles from './index.module.css'
+import authenticate from '../../utils/authenticate'
+import UserContext from '../../Context'
 
-const Login = (username, password) => {
+const Login = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const context = useContext(UserContext)
+    const history = useHistory()
+
+    const handleSubmit = async (event) => {
+        event.preventDefault()
+
+        await authenticate('http://localhost:9999/api/user/login', {
+            email,
+            password
+        }, (user) => {
+            context.logIn(user)
+            history.push('/')
+        }, (e) => {
+            console.log('Error', e)
+        }
+        )
+    }
+
     return (
         <section className="login">
             <Title title="Вход:" />
-            <form className={styles.login} onSubmit='onSubmit'>
+            <form className={styles.login} onSubmit={handleSubmit}>
                 <fieldset>
                     <Input
                         label="Имейл"
                         id="email"
-                        value="email"
-                        onChange="onChange"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
                     />
                     <Input
                         label="Парола"
                         type="password"
                         id="password"
-                        value="password"
-                        onChange="onChange"
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
                     />
                     <Button
                         type="submit"
--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -1,18 +1,60 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Post from '../../components/postCard'
 import Recipe from '../../components/recipeCard'
 // import '../../App.css'
 
-const Profile = () => {
-    return (
-        <section >
-            <p>Username: username</p>
-            <p >Has recipes.length recipes </p>
-            <Recipe />
-            <p >Has posts.length posts </p>
-            <Post />
-        </section>
-    )
+class Profile extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            username: null,
+            posts: [],
+            recipes: []
+        }
+    }
+
+    getUser = async (id) => {
+
+        const promise = await fetch(`http://localhost:9999/api/user?id=${id}`)
+        const user = await promise.json()
+
+        console.log(user);
+        this.setState({
+            username: user.username,
+            posts: user.posts && user.posts.length,
+            recipes: user.recipes && user.recipes.length
+        })
+    }
+
+    // renderPosts() {
+    //     const { posts } = this.state
+
+    //     return posts.map(post => {
+    //         return (
+    //             <Post key={post._id} {...post} />
+    //         )
+    //     })
+    // }
+
+    componentDidMount() {
+        this.getUser(this.props.match.params.userId)
+    }
+
+
+    render() {
+
+        return (
+            <section >
+                <p>Username: username</p>
+                <p >Has recipes.length recipes </p>
+                <Recipe />
+                <p >Has posts.length posts </p>
+                <Post />
+            </section>
+        )
+    }
 }
 
 export default Profile
