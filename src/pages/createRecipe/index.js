4608895d7a83fbaed157fe6f0e5b43fd10614731
Boryana Georgieva <borqna_georgieva@abv.bg>
2021-04-14 23:55:12 +0300

    add create recipe page

--- src/pages/createRecipe/index.js
+++ src/pages/createRecipe/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from '../../components/button'
 import Input from '../../components/input'
 import Textarea from '../../components/textarea'
@@ -7,6 +7,24 @@ import './index.module.css'
 
 const CreateRecipe = () => {
 
+    const [recipe, setRecipe] = useState('')
+    const [updatedRecipe, setUpdatedRecipe] = useState([])
+
+    const handleSubmit = async () => {
+        await fetch('http://localhost:9999/api/recipe', {
+            method: 'POST',
+            body: JSON.stringify({
+                description: recipe
+            }),
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': getCookie('x-auth-token')
+            }
+        })
+
+        setRecipe('')
+        setUpdatedRecipe([...updatedRecipe, 1])
+    }
 
     return (
         <section className="createRecept">
@@ -16,35 +34,40 @@ const CreateRecipe = () => {
                     <Input
                         label="Име на рецептата"
                         id="recipeTitle"
-                        value="recipeTitle"
-                    // onChange="onChange"
+                        value={recipeTitle}
+                        onChange={e => setRecipe(e.target.value)}
                     />
                     <Input
                         label="Категория"
                         id="category"
-                        value="category"
-                    // onChange="onChange"
+                        value={category}
+                        onChange={e => setRecipe(e.target.value)}
                     />
                     <Textarea
                         label="Необходими продукти"
                         id="products"
+                        value={products}
+                        onChange={e => setRecipe(e.target.value)}
                         placeholder="Въведете необходимите продукти"
                     />
                     <Textarea
                         label="Начин на приготвяне"
                         id="made"
+                        value={made}
+                        onChange={e => setRecipe(e.target.value)}
                         placeholder="Въведете начина на приготвяне"
                     />
                     <Input
                         id="recipeImg"
                         label="Снимка"
                         type="image/jpeg"
-                        value="recipeImg"
+                        onChange={e => setRecipe(e.target.value)}
+                        value={recipeImg}
                     />
                     <Button
                         type="submit"
                         title="Добави"
-                        className="createRecept"
+                        onClick={handleSubmit}
                     />
 
                 </fieldset>
